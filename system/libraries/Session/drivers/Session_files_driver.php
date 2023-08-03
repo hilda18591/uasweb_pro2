1761e5efafe28471aee384a5737161f338076344
hilda18591 <140933144+hilda18591@users.noreply.github.com>
2023-08-03 20:40:40 +0700

    Update Session_files_driver.php

--- system/libraries/Session/drivers/Session_files_driver.php
+++ system/libraries/Session/drivers/Session_files_driver.php
@@ -128,7 +128,7 @@ class CI_Session_files_driver extends CI_Session_driver implements SessionHandle
 	 * @param	string	$name		Session cookie name
 	 * @return	bool
 	 */
-	#[\ReturnTypeWillChange]
+	// #[\ReturnTypeWillChange]
 	public function open($save_path, $name)
 	{
 		if (!is_dir($save_path)) {
@@ -159,7 +159,7 @@ class CI_Session_files_driver extends CI_Session_driver implements SessionHandle
 	 * @param	string	$session_id	Session ID
 	 * @return	string	Serialized session data
 	 */
-	#[\ReturnTypeWillChange]
+	// #[\ReturnTypeWillChange]
 	public function read($session_id)
 	{
 		// This might seem weird, but PHP 5.6 introduces session_reset(),
@@ -220,7 +220,7 @@ class CI_Session_files_driver extends CI_Session_driver implements SessionHandle
 	 * @param	string	$session_data	Serialized session data
 	 * @return	bool
 	 */
-	#[\ReturnTypeWillChange]
+	// #[\ReturnTypeWillChange]
 	public function write($session_id, $session_data)
 	{
 		// If the two IDs don't match, we have a session_regenerate_id() call
@@ -269,7 +269,7 @@ class CI_Session_files_driver extends CI_Session_driver implements SessionHandle
 	 *
 	 * @return	bool
 	 */
-	#[\ReturnTypeWillChange]
+	// #[\ReturnTypeWillChange]
 	public function close()
 	{
 		if (is_resource($this->_file_handle)) {
@@ -292,7 +292,7 @@ class CI_Session_files_driver extends CI_Session_driver implements SessionHandle
 	 * @param	string	$session_id	Session ID
 	 * @return	bool
 	 */
-	#[\ReturnTypeWillChange]
+	// #[\ReturnTypeWillChange]
 	public function destroy($session_id)
 	{
 		if ($this->close() === $this->_success) {
@@ -329,7 +329,7 @@ class CI_Session_files_driver extends CI_Session_driver implements SessionHandle
 	 * @param	int 	$maxlifetime	Maximum lifetime of sessions
 	 * @return	bool
 	 */
-	#[\ReturnTypeWillChange]
+	// #[\ReturnTypeWillChange]
 	public function gc($maxlifetime)
 	{
 		if (!is_dir($this->_config['save_path']) or ($directory = opendir($this->_config['save_path'])) === FALSE) {
